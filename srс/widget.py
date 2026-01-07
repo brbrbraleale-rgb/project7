ca1be27e423550133c1587f1953e20eec57b72da
Безгина Анастасия <brbrbraleale@gmail.com>
2026-01-07 03:39:16 +0300

    в модуль widget.py добавлена функция преобразования даты в нужныый формат

--- srс/widget.py
+++ srс/widget.py
@@ -20,3 +20,21 @@ def mask_account_card(number: str) -> str:
 # Пример:
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
+
+def get_date(date_string: str) -> str:
+    """
+    Принимает строку с датой в формате '2024-03-11T02:26:18.671407'
+    и возвращает дату в формате 'ДД.ММ.ГГГГ/
+"11.03.2024")'.
+    """
+    # Извлекаем год, месяц и день с помощью срезов
+    year = date_string[0:4]
+    month = date_string[5:7]
+    day = date_string[8:10]
+
+    # Собираем в нужном порядке
+    return f"{day}.{month}.{year}"
+
+# Пример:
+date_ = "2024-03-11T02:26:18.671407"
+print(get_date(date_))
