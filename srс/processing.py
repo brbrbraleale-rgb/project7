ffbd1d05d51900a70cac87727230a8f25b182321
Безгина Анастасия <brbrbraleale@gmail.com>
2026-01-28 03:17:41 +0300

    модуль "processing.py" дополнен функцией сортировки по дате

--- srс/processing.py
+++ srс/processing.py
@@ -11,3 +11,11 @@ def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list
         if item.get('state') == state:
             result.append(item)
     return result
+
+def sort_by_date(data: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """
+    Функция принимает список словарей и возвращает новый список,
+    отсортированный по ключу 'date'.
+    """
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)
+
